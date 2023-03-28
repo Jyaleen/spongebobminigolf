701ba0897b3968680914c953960fc28c3075a56e
Jyaleen <47906710+Jyaleen@users.noreply.github.com>
2023-03-28 00:16:43 -0400

    put servo and distance sensor code together

--- minigolf.ino
+++ minigolf.ino
@@ -6,11 +6,28 @@ int servoDelay = 15;
 
 Servo myServo;              //create a servo object
 
-void setup() {
+const int trigPin = 11;           //connects to the trigger pin on the distance sensor
+const int echoPin = 12;           //connects to the echo pin on the distance sensor
 
-  Serial.begin(9600);
+const int redPin = 3;             //pin to control the red LED inside the RGB LED
+const int greenPin = 5;           //pin to control the green LED inside the RGB LED
+const int bluePin = 6;            //pin to control the blue LED inside the RGB LED
+
+float distance = 0;               //stores the distance measured by the distance sensor
+
+void setup()
+{
+  Serial.begin (9600);        //set up a serial connection with the computer
+
   myServo.attach(servoPin);        //tell the servo object that its servo is plugged into pin 9
 
+  pinMode(trigPin, OUTPUT);   //the trigger pin will output pulses of electricity
+  pinMode(echoPin, INPUT);    //the echo pin will measure the duration of pulses coming back from the distance sensor
+
+  //set the RGB LED pins to output
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
@@ -24,5 +41,50 @@ void loop() {
     myServo.write(pos);
     delay(servoDelay);
   }
+
+  distance = getDistance();   //variable to store the distance measured by the sensor
+
+  Serial.print(distance);     //print the distance that was measured
+  Serial.println(" in");      //print units after the distance
+
+  if (distance <= 10) {                       //if the object is close
+
+    //make the RGB LED green
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 0);
+
+  } 
+  else {                                    //if the object is far away
+
+    //no rgb
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
+  }
+
+  delay(50);      //delay 50ms between each reading
 }
+
+//------------------FUNCTIONS-------------------------------
+
+//RETURNS THE DISTANCE MEASURED BY THE HC-SR04 DISTANCE SENSOR
+float getDistance()
+{
+  float echoTime;                   //variable to store the time it takes for a ping to bounce off an object
+  float calculatedDistance;         //variable to store the distance calculated from the echo time
+
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  echoTime = pulseIn(echoPin, HIGH);      //use the pulsein command to see how long it takes for the
+                                          //pulse to bounce back to the sensor
+
+  calculatedDistance = echoTime / 148.0;  //calculate the distance of the object that reflected the pulse (half the bounce time multiplied by the speed of sound)
+
+  return calculatedDistance;              //send back the distance that was calculated
+}
+
 
