48d117596618d8668c2fb7035f83e8a904319136
Jyaleen <47906710+Jyaleen@users.noreply.github.com>
2023-03-28 00:08:58 -0400

    Update distance.ino

--- distance.ino
+++ distance.ino
@@ -1,22 +1,8 @@
-/*
-  SparkFun Inventor’s Kit
-  Circuit 3B-Distance Sensor
-
-  Control the color of an RGB LED using an ultrasonic distance sensor.
-
-  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
-  This code is completely free for any use.
-
-  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v41
-  Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
-*/
 
 const int trigPin = 11;           //connects to the trigger pin on the distance sensor
 const int echoPin = 12;           //connects to the echo pin on the distance sensor
 
-const int redPin = 3;             //pin to control the red LED inside the RGB LED
 const int greenPin = 5;           //pin to control the green LED inside the RGB LED
-const int bluePin = 6;            //pin to control the blue LED inside the RGB LED
 
 float distance = 0;               //stores the distance measured by the distance sensor
 
@@ -28,9 +14,7 @@ void setup()
   pinMode(echoPin, INPUT);    //the echo pin will measure the duration of pulses coming back from the distance sensor
 
   //set the RGB LED pins to output
-  pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
@@ -42,24 +26,9 @@ void loop() {
   if (distance <= 10) {                       //if the object is close
 
     //make the RGB LED red
-    analogWrite(redPin, 255);
-    analogWrite(greenPin, 0);
-    analogWrite(bluePin, 0);
-
-  } else if (10 < distance && distance < 20) { //if the object is a medium distance
-
-    //make the RGB LED yellow
-    analogWrite(redPin, 255);
-    analogWrite(greenPin, 50);
-    analogWrite(bluePin, 0);
-
-  } else {                                    //if the object is far away
-
-    //make the RGB LED green
-    analogWrite(redPin, 0);
     analogWrite(greenPin, 255);
-    analogWrite(bluePin, 0);
-  }
+
+  } 
 
   delay(50);      //delay 50ms between each reading
 }
@@ -84,4 +53,3 @@ float getDistance()
 
   return calculatedDistance;              //send back the distance that was calculated
 }
-
